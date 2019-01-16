d157e3489d5529e3cb64f5fcdb2d4f6ca65848bf
Sina Mahmoodi <itz.s1na@gmail.com>
2019-01-16 15:01:52 +0100

    Add comments to scratch
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/scratch.js
+++ src/scratch.js
@@ -5,12 +5,22 @@ const { asyncFirstSeries } = require('./util/async')
 
 const ENCODING_OPTS = { keyEncoding: 'binary', valueEncoding: 'binary' }
 
+/**
+ * An in-memory wrap over `DB` with a backend DB
+ * which will be queried when a key is not found
+ * in the in-memory scratch. This class is used to implement
+ * checkpointing functionality in CheckpointTrie.
+ */
 module.exports = class ScratchDB extends DB {
   constructor (db) {
     super()
     this._backend = db._db
   }
 
+  /**
+   * Similar to `DB.get`, but first searches in-memory
+   * scratch DB, if key not found, searches backend DB.
+   */
   get (key, cb) {
     const getDBs = this._backend ? [this._db, this._backend] : [this._db]
     const dbGet = (db, cb2) => {
